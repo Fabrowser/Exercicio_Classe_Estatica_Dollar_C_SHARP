a97c633d45d160d451e98419e8943a773fc882c9
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-10 13:31:06 -0300

    Add files via upload

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioClasse_Dollar
+{
+     class ConversorDeMoeda
+    {
+        public static double ValorDolares;
+        public static double Cotacao;
+        public static double Iof = 6.0;
+
+
+        public static double CalculaValorReais()
+        {
+
+
+            double valor_reais = ((ValorDolares * Cotacao));
+
+            double total_pago_reais = valor_reais + (valor_reais * Iof) / 100;
+
+            return total_pago_reais;
+
+        }
+
+        public static void RecebeValores(double dolares, double cotacao)
+        {
+            Cotacao = cotacao;
+            ValorDolares = dolares;
+
+        }
+      
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExercicioClasse_Dollar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Digite a cotação do dólar:");
+            double cotacao = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite o valor em dólar a ser comprado:");
+            double dolar = double.Parse(Console.ReadLine());
+
+    
+
+
+            ConversorDeMoeda.RecebeValores(dolar, cotacao);
+
+            ConversorDeMoeda.CalculaValorReais();
+
+            Console.WriteLine($"Cotação:{ConversorDeMoeda.Cotacao.ToString("F2")} \nCompra em Dólar: " +
+                $"{ConversorDeMoeda.ValorDolares.ToString("F2")}" +
+                $"\nTotal a ser pago em Reais: {ConversorDeMoeda.CalculaValorReais().ToString("F2")}");
+            
+
+
+        }
+    }
+}
